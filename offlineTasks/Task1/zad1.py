ed3a183f63c0aa57e8b4f65bc4a5504fda952b33
Michal0ss <michal.bialas.pol@gmail.com>
2025-03-14 15:17:04 +0100

    completed first offline task

--- offlineTasks/Task1/zad1.py
+++ offlineTasks/Task1/zad1.py
@@ -1,5 +1,3 @@
-from pydoc import visiblename
-
 from zad1testy import runtests
 
 # Mówimy, że dwa napisy są sobie równoważne, jeśli albo są identyczne, albo byłyby identyczne,
@@ -19,48 +17,65 @@ from zad1testy import runtests
 # wywołanie g(T) powinno zwrócić 3. Algorytm powinien być możliwie jak najszybszy. Proszę podać
 # złożoność czasową i pamięciową zaproponowanego algorytmu.
 
-def reversed_str(s):
-    return s[::-1]
+
+def merge_sort(arr):
+    n=len(arr)
+    if n<=1:
+        return arr
+    mid = n//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    sorted_arr = []
+    i=j=0
+
+    #merge
+    while i<len(left) and j<len(right):
+        if left[i] <= right[j]:
+            sorted_arr.append(left[i])
+            i+=1
+        else:
+            sorted_arr.append(right[j])
+            j+=1
+
+    #scal pozostala reszte
+    while i<len(left):
+        sorted_arr.append(left[i])
+        i+=1
+
+    while j<len(right):
+        sorted_arr.append(right[j])
+        j+=1
+    return sorted_arr
+
 
 def strong_string(T):
-    """korzystajac z mniej leksykograficznej wersji napisu i z wykorzystaniem slownika str_power
-        tworze porownania o zlozonosci O(n)"""
+    """Uzywamy sortowanie merge sort o zlozonosci NlogN aby przy pomocy kolejnosci leksykograficznej
+    posortowac nasz liste a nastepnie normalizujemy nasze wyrazy przy pomocy funkci min() sprawdzajac
+    co ma mniejsza wartosc leksykograficzna np wyraz kot czy tok odwracajac go za pomoca slicingu [::-1], wowczas
+    zostanie nam posortowana lista o powtarzajacych sie wyrazach np: [kot, kot, kot, pies, pies]
+
+    Na koniec liczymy moc kazdego wyrazu jedna petla na od 1 do n zatem koncowa zlozonosc to NlogN
+    poniewaz w przypadku calego programu O(n) + O(nlogn) + O(n) = O(nlogn)"""
 
     n=len(T)
-    str_power = {}
 
-    for element in T:
-        key = min(element, reversed_str(element))
-        str_power[key] = str_power.get(key, 0) + 1  # we are retrieving  value from dictionary and adding 1
-                                                    # syntax: .get(our key, value returned if key not found)
-    return max(str_power.values()) # maximum power of a single str
+    norml=[ min(s,s[::-1]) for s in T] # normalizuje cala tablice
+    sorted_normalize = merge_sort(norml) #nastepnie za pomoca scalania ja sortuje
 
+    max_counter=1
+    current = 0
 
-#def huge_complexity(T):
-#    """method with 0(n^2) complexity just for self education how not to build algorithms"""
-#    n = len(T)
-#    max_amount = -1  # Stores the max strength of a string
-#
-#    visited = [0 for _ in range(n)]  # Track visited elements
-#
-#    for i in range(n):
-#        if visited[i] == 1:  # Skip already counted elements
-#            continue
-#
-#        count = 1  # Start counting with the current element
-#        visited[i] = 1  # Mark as visited
-#
-#        for j in range(i + 1, n):  # Check the rest of the list
-#            if visited[j] == 0 and (T[i] == T[j] or reversed_str(T[i]) == T[j]):
-#                count += 1
-#                visited[j] = 1  # Mark matched element as visited
-#
-#        max_amount = max(max_amount, count)  # Update max strength
-#
-#    return max_amount
+    for i in range(1,n):
+        if sorted_normalize[i-1]==sorted_normalize[i]:
+            current+=1
+        else:
+            max_counter = max(max_counter,current)
+            current=1
+    max_counter=max(max_counter,current)
+
+    return max_counter
 
 
-# Zakomentuj gdy uruchamiasz duze testy
-runtests( strong_string, all_tests=False )
-# Odkomentuj by uruchomic duze testy
-# runtests( strong_string, all_tests=True )
+
+runtests( strong_string, all_tests=True )
