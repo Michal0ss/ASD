6bc64b0965f5f958219ff409ecd5459a8b56d11d
Michal0ss <michal.bialas.pol@gmail.com>
2025-04-24 14:09:43 +0200

    added offline tasks

---
+++ GraphsAlg/Kolos/23/kol_main.py
@@ -0,0 +1,17 @@
+from kol2testy import runtests
+
+
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.parent = self
+        self.rank = 0
+    #
+#end Class
+
+def beautree(G):
+    # tu prosze wpisac wlasna implementacje
+    return None
+
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( beautree, all_tests = False )
