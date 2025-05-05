6a8b781076802a549a1600f45443af884998c581
Michal0ss <michal.bialas.pol@gmail.com>
2025-05-06 00:04:28 +0200

    added task nr5

---
+++ offlineTasks/Graph/task_5/zad.py
@@ -0,0 +1,8 @@
+from zadtesty import runtests
+
+def goodknight( G, s, t ):
+  # tu prosze wpisac wlasna implementacje
+  pass
+
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( goodknight, all_tests = False )
