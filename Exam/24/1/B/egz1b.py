6ab70b2f1cfeb37c7d5ed081664d56c30d1ad787
Michal0ss <michal.bialas.pol@gmail.com>
2025-06-16 20:32:21 +0200

    completed exam 24 1A

--- Exam/24/1/B/egz1b.py
+++ Exam/24/1/B/egz1b.py
@@ -1,9 +1,22 @@
+from math import inf
+
 from egz1btesty import runtests
 
 
 def kstrong( T, k):
-  # tu prosze wpisac wlasna implementacje
-  return -1
+  n = len(T)
+  bestof = [[None] * (k + 1) for _ in range(n)]
+
+  def p(i,j):
+    if bestof[i][j] == None:
+      bestof[i][j] = max(
+        T[i], # bierzemy aktualna (zaczynamy ciag)
+        p(i-1, j-1) if i > 0 and j> 0 else -inf, # pomijamy liczbe
+        T[i] + p(i-1, j) if i > 0 else -inf # bierzemy liczbe i dodajemy do ciagu
+      )
+      return bestof[i][j]
+  return max(p(i,k) for i in range(n))
+
 
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
