0c1df5d4d997bd635eff03f1da583a85bc8e12cc
LAPTOP_MICHAL\Michal0ss <michal.bialas.pol@gmail.com>
2025-06-20 15:40:09 +0200

    Added exams

--- Exam/24/3/A/egz3a.py
+++ Exam/24/3/A/egz3a.py
@@ -1,9 +1,30 @@
-from egz3atesty import runtests
+from math import inf
 
+from egz3atesty import runtests
+from collections import deque
+
+def bfs(adj_g, T, n):
+  V = len(adj_g)
+  visited_time = [inf] * V
+  tree_owner = [-1] * V
+
+  q = deque()
+
+  for i in range(len(T)):
+    u = T[i]
+    q.append(u)
+
+  while q:
+    curr = q.popleft()
+    t= visited_time[curr] + 1
+    for x in adj_g[curr]:
+      if visited_time[x] > t:
+        visited[x] = True
+        q.append(x)
+  ...
 
 def mykoryza( G,T,d ):
-  # tu prosze wpisac wlasna implementacje
-  return 0
+  ...
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( mykoryza, all_tests = False )
