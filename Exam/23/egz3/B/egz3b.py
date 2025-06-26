804269447155c626176dcbd190a1a29c33da6a7c
Michal0ss <michal.bialas.pol@gmail.com>
2025-06-26 13:33:12 +0200

    completed exams

--- Exam/23/egz3/B/egz3b.py
+++ Exam/23/egz3/B/egz3b.py
@@ -1,8 +1,24 @@
 from egz3btesty import runtests
 
-def uncool( P ):
-  # tu prosze wpisac wlasna implementacje
-  pass
+def uncool(P):
+    n = len(P)
+    for i in range(n):
+        for j in range(i + 1, n):
+            a1, b1 = P[i]
+            a2, b2 = P[j]
+
+            # Sprawdź czy są rozłączne
+            if b1 < a2 or b2 < a1:
+                continue  # są fajne
+
+            # Sprawdź czy jeden zawiera się w drugim
+            if (a1 <= a2 and b1 >= b2) or (a2 <= a1 and b2 >= b1):
+                continue  # są fajne
+
+            # Jeśli nie są rozłączne i żaden nie zawiera się w drugim -> niefajna para
+            return (i, j)
+
+    return None  # zgodnie z treścią, zawsze istnieje jakaś para niefajna
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( uncool, all_tests = False )
+runtests(uncool, all_tests=True)
--- Exam/24/3/B/egz3b.py
+++ Exam/24/3/B/egz3b.py
@@ -1,5 +1,13 @@
 from egz3btesty import runtests
 
+"""
+Wygenerować wszystkie k-pechowe liczby, aż do max(T) (żeby wiedzieć, które elementy tablicy T są k-pechowe).
+
+Przesuwać okno (left, right) po tablicy i pilnować, żeby w środku były najwyżej 2 k-pechowe liczby.
+
+Zwrócić długość najdłuższego takiego fragmentu.
+
+"""
 
 def kunlucky(T, k):
   # tu prosze wpisac wlasna implementacje
