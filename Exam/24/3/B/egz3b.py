f7ae563607ddfb53f7d43038eb56a0961fd5c675
LAPTOP_MICHAL\Michal0ss <michal.bialas.pol@gmail.com>
2025-06-26 16:20:44 +0200

    completed exam 3b 24

--- Exam/24/3/B/egz3b.py
+++ Exam/24/3/B/egz3b.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from math import inf
+
 from egz3btesty import runtests
 
 """
@@ -7,11 +10,62 @@ Przesuwać okno (left, right) po tablicy i pilnować, żeby w środku były najw
 
 Zwrócić długość najdłuższego takiego fragmentu.
 
+left – początek okna
+
+right – koniec okna (idziemy nim do przodu)
+
+count – liczba k-pechowych w bieżącym oknie
+
+Idea:
+Przesuwasz right do przodu
+
+Jeśli T[right] jest k-pechowa, zwiększasz licznik count
+
+Jeśli count > 2, to przesuwasz left do przodu, aż znowu w oknie będą najwyżej 2 k-pechowe
+
+Aktualizujesz max_len = max(max_len, right - left + 1)
+
 """
 
+# O(n)
+
+from egz3btesty import runtests
+
+def gen_kunlucky_set(T, k, max_num):
+    kset = set()
+    x = k
+    i = 1
+
+    while x <= max_num:
+        kset.add(x)
+        x = x + (x % i) + 7
+        i += 1
+
+    return kset
+
 def kunlucky(T, k):
-  # tu prosze wpisac wlasna implementacje
-  return -1
+    n = len(T)
+
+    kset = gen_kunlucky_set(T, k, n) # liczby z zakresu [1, ..., n], więc max_num = n
+
+    i, j = 0, 0
+    cnt, tmp_len, max_len = 0, 0, 0
+
+    while j < n:
+      # zamieniam kazda wartosc z tablicy T po ktorej przejde w zaleznosci czy jest pechwoa na 1 lub 0
+        if cnt < 3:
+            T[j] = T[j] in kset # 1 lub 0
+            tmp_len += 1
+            cnt += T[j]
+            j += 1
+            if cnt < 3 and max_len < tmp_len: max_len = tmp_len
+        else:
+            tmp_len -= 1
+            cnt -= T[i]
+            i += 1
+
+    return max_len
+
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( kunlucky, all_tests = False )
+runtests( kunlucky, all_tests = True )
